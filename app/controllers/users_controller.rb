b61015f2b784b773d26ab12f2465fb041a4152c8
Jason Matthews <jmatty1983@gmail.com>
2015-03-11 16:42:43 +0000

    Add gravatar profile image

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,5 +4,11 @@ class UsersController < ApplicationController
   end
 
   def show
+    if current_user.id == params[:id]
+      @user = current_user
+      render 'user/profile'
+    else
+      @user = User.find_by(id: params[:id])
+    end
   end
 end
--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -6,4 +6,11 @@ module UsersHelper
   def unlike_tag
     content_tag :span, " unlike", class: "glyphicon glyphicon-thumbs-down"
   end
+  
+  def gravatar_for(user, options = { size: 80 })
+    gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+    size = options[:size]
+    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
+    image_tag(gravatar_url, alt: user.name, class: "gravatar")
+  end
 end
