1fd8d4b5490a5e800c72aa01fae782bbe79ebc8a
Jason Matthews <jmatty1983@gmail.com>
2015-03-11 17:27:54 +0000

    clean up post feed views

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,4 @@
-email = 'kid.bytes@gmail.com'
+email = 'test@test.com'
 User.create!(email: email, name: 'Jason M', password: "password", password_confirmation: "password")
 
 50.times do
@@ -11,6 +11,13 @@ requester = User.first
 users = User.limit(15).offset(1).order(:created_at)
 users.each_with_index do |u, i|
   Friendship.create!(requester: requester, requested: u, accepted: i % 2) unless requester.id == u.id
+end
+
+
+7.times do
+  users.each do |u|
+    u.posts.create!(content: Faker::Lorem.sentence)
+  end
 end
 
 users = User.limit(10).offset(16).order(:created_at)
